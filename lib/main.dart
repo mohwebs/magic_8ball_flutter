2b19053ffaf6da648992e3120be21b7952e63b8c
Nehari <mohwebs@gmail.com>
2021-05-29 18:16:48 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key key}) : super(key: key);
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text("Ask Me Anything"),
+      ),
+      body: Ball()
+    );
+  }
+}
+class Ball extends StatefulWidget {
+  const Ball({Key key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child:
+      FlatButton(child: Image.asset('images/ball1.png'),
+      onPressed: (){
+        print('i got pressed');
+    },
+      ),
+    );
+  }
+}
+
+
